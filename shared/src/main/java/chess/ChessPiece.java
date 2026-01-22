b1e2fe28d5a629d4862f62389d04fd76e31dcd5e
cyber-sec0 <91571849+cyber-sec0@users.noreply.github.com>
2026-01-22 20:34:16 +0000

    .

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -3,12 +3,6 @@ package chess;
 import java.util.Collection;
 import java.util.Objects;
 
-/**
- * Represents a single chess piece
- * <p>
- * Note: You can add to this class, but you may not alter
- * signature of the existing methods.
- */
 public class ChessPiece {
 
     private final ChessGame.TeamColor pieceColor;
@@ -19,9 +13,6 @@ public class ChessPiece {
         this.type = type;
     }
 
-    /**
-     * The various different chess piece options
-     */
     public enum PieceType {
         KING,
         QUEEN,
@@ -31,27 +22,14 @@ public class ChessPiece {
         PAWN
     }
 
-    /**
-     * @return Which team this chess piece belongs to
-     */
     public ChessGame.TeamColor getTeamColor() {
         return pieceColor;
     }
 
-    /**
-     * @return which type of chess piece this piece is
-     */
     public PieceType getPieceType() {
         return type;
     }
 
-    /**
-     * Calculates all the positions a chess piece can move to
-     * Does not take into account moves that are illegal due to leaving the king in
-     * danger
-     *
-     * @return Collection of valid moves
-     */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         PieceMovesCalculator calculator;
 
@@ -62,9 +40,20 @@ public class ChessPiece {
             case ROOK:
                 calculator = new RookMovesCalculator();
                 break;
-            // Add other cases as you implement them (BISHOP, KNIGHT, QUEEN, PAWN)
+            case BISHOP:
+                calculator = new BishopMovesCalculator();
+                break;
+            case KNIGHT:
+                calculator = new KnightMovesCalculator();
+                break;
+            case QUEEN:
+                calculator = new QueenMovesCalculator();
+                break;
+            case PAWN:
+                calculator = new PawnMovesCalculator();
+                break;
             default:
-                throw new RuntimeException("Not implemented yet");
+                throw new RuntimeException("Unknown piece type");
         }
 
         return calculator.calculateMoves(board, myPosition, this);
