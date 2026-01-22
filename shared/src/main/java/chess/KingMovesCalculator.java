94d2675e4fb97207631df3470489b3ad72ee9e1a
cyber-sec0 <91571849+cyber-sec0@users.noreply.github.com>
2026-01-22 04:54:03 +0000

    .

--- shared/src/main/java/chess/KingMovesCalculator.java
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -1,29 +1,19 @@
 package chess;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.*;
 
-public class KnightMovesCalculator implements PieceMovesCalculator {
+public class KingMovesCalculator implements PieceMovesCalculator {
     @Override
     public Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition myPosition, ChessPiece piece) {
         Collection<ChessMove> moves = new ArrayList<>();
-        int row = myPosition.getRow();
-        int col = myPosition.getColumn();
-
-        // L-shapes
-        int[][] directions = {
-            {2, 1}, {2, -1}, {-2, 1}, {-2, -1},
-            {1, 2}, {1, -2}, {-1, 2}, {-1, -2}
-        };
+        int[][] directions = {{1,0},{-1,0},{0,1},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
 
         for (int[] dir : directions) {
-            int newRow = row + dir[0];
-            int newCol = col + dir[1];
-
+            int newRow = myPosition.getRow() + dir[0];
+            int newCol = myPosition.getColumn() + dir[1];
             if (newRow >= 1 && newRow <= 8 && newCol >= 1 && newCol <= 8) {
                 ChessPosition newPos = new ChessPosition(newRow, newCol);
                 ChessPiece pieceAtPos = board.getPiece(newPos);
-
                 if (pieceAtPos == null || pieceAtPos.getTeamColor() != piece.getTeamColor()) {
                     moves.add(new ChessMove(myPosition, newPos, null));
                 }
