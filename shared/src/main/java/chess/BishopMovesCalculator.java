2527e47ceb252f508135e3b8f9c3227758bb5c6b
cyber-sec0 <91571849+cyber-sec0@users.noreply.github.com>
2026-01-29 10:35:20 -0800

    .

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -2,11 +2,17 @@ package chess;
 
 import java.util.Collection;
 
+/**
+ * Calculates the movement vectors for the Bishop unit.
+ * The Bishop is restricted to diagonal pathways.
+ */
 public class BishopMovesCalculator extends SlidingMovesCalculator {
     @Override
     public Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition myPosition, ChessPiece piece) {
-        // Bishops only move in diagonals
-        int[][] diagonalWays = {{1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
-        return runSlidingLogic(board, myPosition, piece, diagonalWays);
+        // Defining the 4 diagonal vectors
+        int[][] diagonalVectors = {{1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+
+        // Initiating the diagonal scan protocol
+        return scanDirectionalPath(board, myPosition, piece, diagonalVectors);
     }
 }
