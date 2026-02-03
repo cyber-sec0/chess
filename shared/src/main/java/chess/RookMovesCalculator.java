afcd6c956f26e697de7e1b94b8a7159c6d6d60c9
cyber-sec0 <91571849+cyber-sec0@users.noreply.github.com>
2026-02-03 17:34:51 +0000

    .

--- shared/src/main/java/chess/RookMovesCalculator.java
+++ shared/src/main/java/chess/RookMovesCalculator.java
@@ -2,11 +2,17 @@ package chess;
 
 import java.util.Collection;
 
+/**
+ * Calculates the movement vectors for the Rook unit.
+ * The Rook operates on orthogonal axes (vertical and horizontal).
+ */
 public class RookMovesCalculator extends SlidingMovesCalculator {
     @Override
     public Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition myPosition, ChessPiece piece) {
-        // Rooks move in straight lines (up, down, left, right)
-        int[][] straightWays = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        return runSlidingLogic(board, myPosition, piece, straightWays);
+        // Defining the 4 orthogonal vectors: Up, Down, Left, Right
+        int[][] straightLineVectors = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+        // Delegating the scanning operation to the parent security class
+        return scanDirectionalPath(board, myPosition, piece, straightLineVectors);
     }
 }
