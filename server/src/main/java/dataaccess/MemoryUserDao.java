1c1a096eb1491b6e37fe86514b990e7c4fe10551
cyber-sec0 <91571849+cyber-sec0@users.noreply.github.com>
2026-03-03 11:27:22 -0800

    .

--- server/src/main/java/dataaccess/MemoryUserDao.java
+++ server/src/main/java/dataaccess/MemoryUserDao.java
@@ -1,46 +1,103 @@
 package dataaccess;
 
 import model.UserData;
-import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.mindrot.jbcrypt.BCrypt;
 
 /**
- * This class is the implementation of the data access for users in the memory of the computer.
- * It uses a list to store the users which is very simple to understand.
+ * This class is the implementation of the data access for users in the database of the computer.
+ * It uses mysql to store the users which is very safe to not loss data when computer turns off.
  */
 public class MemoryUserDao {
 
-    /**
-     * The list where all the users are going to be stored during the execution of the program.
-     */
-    private static final ArrayList<UserData> DATABASE_OF_USERS_LIST = new ArrayList<>();
+    // This constructor initialize the database connection and creates table if not exist
+    public MemoryUserDao() {
+        try {
+            DatabaseManager.createDatabase();
+            String createTableStringCommandExecution = "CREATE TABLE IF NOT EXISTS usersDatabaseTableForStorage (" +
+                    "usernameStringForDatabase VARCHAR(255) NOT NULL, " +
+                    "passwordStringForDatabase VARCHAR(255) NOT NULL, " +
+                    "emailStringForDatabase VARCHAR(255) NOT NULL, " +
+                    "PRIMARY KEY (usernameStringForDatabase))";
+            try (Connection connectionToDatabaseNetwork = DatabaseManager.getConnection()) {
+                try (PreparedStatement preparedStatementForExecutionCommand = connectionToDatabaseNetwork.prepareStatement(createTableStringCommandExecution)) {
+                    preparedStatementForExecutionCommand.executeUpdate();
+                }
+            }
+        } catch (DataAccessException | SQLException exceptionFromDatabaseNetworkError) {
+            throw new RuntimeException("Error: " + exceptionFromDatabaseNetworkError.getMessage());
+        }
+    }
 
     /**
-     * This function has the responsibility to find a user inside the list by using the name.
-     * It iterates over every single user to check if the name matches the parameter.
-     * @param usernameToFind The name of the user that needs to be found.
-     * @return The UserData object if found, otherwise it returns null.
+     * This function gets the user object from the database table using the name of the user.
+     * It executes a select statement to find the correct line in the table.
+     * @param usernameToFindInDatabaseTable The string name to search.
+     * @return The user data object or null.
+     * @throws DataAccessException if connection fails so server can return 500 error code.
      */
-    public UserData getUser(String usernameToFind) {
-        for (UserData currentUserInLoop : DATABASE_OF_USERS_LIST) {
-            if (currentUserInLoop.username().equals(usernameToFind)) {
-                return currentUserInLoop;
+    public UserData getUser(String usernameToFindInDatabaseTable) throws DataAccessException {
+        String selectCommandStringForDatabase = "SELECT usernameStringForDatabase, passwordStringForDatabase, emailStringForDatabase FROM usersDatabaseTableForStorage WHERE usernameStringForDatabase = ?";
+        try (Connection connectionToDatabaseNetwork = DatabaseManager.getConnection()) {
+            try (PreparedStatement preparedStatementForExecutionCommand = connectionToDatabaseNetwork.prepareStatement(selectCommandStringForDatabase)) {
+                preparedStatementForExecutionCommand.setString(1, usernameToFindInDatabaseTable);
+                try (ResultSet resultFromDatabaseQueryExecution = preparedStatementForExecutionCommand.executeQuery()) {
+                    // Checking if the result from database has the user line so we dont return empty
+                    if (resultFromDatabaseQueryExecution.next()) {
+                        return new UserData(
+                                resultFromDatabaseQueryExecution.getString("usernameStringForDatabase"),
+                                resultFromDatabaseQueryExecution.getString("passwordStringForDatabase"),
+                                resultFromDatabaseQueryExecution.getString("emailStringForDatabase")
+                        );
+                    }
+                }
             }
+        } catch (SQLException exceptionFromDatabaseNetworkError) {
+            // Throw the data access exception so server handler gives 500 error code for bad connection
+            throw new DataAccessException(exceptionFromDatabaseNetworkError.getMessage());
         }
         return null;
     }
 
     /**
-     * This function is responsible for adding a new user to the internal list storage.
-     * @param newUserData The object containing the user information.
+     * This function creates a new user inside the database table storage.
+     * It hashes the password before saving for security reasons against hackers.
+     * @param newUserDataToSaveInDatabaseTable The object with data.
+     * @throws DataAccessException if connection fails so server can return 500 error code.
      */
-    public void createUser(UserData newUserData) {
-        DATABASE_OF_USERS_LIST.add(newUserData);
+    public void createUser(UserData newUserDataToSaveInDatabaseTable) throws DataAccessException {
+        String insertCommandStringForDatabase = "INSERT INTO usersDatabaseTableForStorage (usernameStringForDatabase, passwordStringForDatabase, emailStringForDatabase) VALUES (?, ?, ?)";
+        try (Connection connectionToDatabaseNetwork = DatabaseManager.getConnection()) {
+            try (PreparedStatement preparedStatementForExecutionCommand = connectionToDatabaseNetwork.prepareStatement(insertCommandStringForDatabase)) {
+                preparedStatementForExecutionCommand.setString(1, newUserDataToSaveInDatabaseTable.username());
+                String hashedPasswordStringForSecurity = BCrypt.hashpw(newUserDataToSaveInDatabaseTable.password(), BCrypt.gensalt());
+                preparedStatementForExecutionCommand.setString(2, hashedPasswordStringForSecurity);
+                preparedStatementForExecutionCommand.setString(3, newUserDataToSaveInDatabaseTable.email());
+                preparedStatementForExecutionCommand.executeUpdate();
+            }
+        } catch (SQLException exceptionFromDatabaseNetworkError) {
+            // Throw the data access exception so server handler gives 500 error code for bad connection
+            throw new DataAccessException(exceptionFromDatabaseNetworkError.getMessage());
+        }
     }
 
     /**
-     * This function clears all the data from the list to reset the database.
+     * This function removes all the data from the user table completely.
+     * It is used for the clear system command.
+     * @throws DataAccessException if connection fails so server can return 500 error code.
      */
-    public void clear() {
-        DATABASE_OF_USERS_LIST.clear();
+    public void clear() throws DataAccessException {
+        String deleteCommandStringForDatabase = "TRUNCATE TABLE usersDatabaseTableForStorage";
+        try (Connection connectionToDatabaseNetwork = DatabaseManager.getConnection()) {
+            try (PreparedStatement preparedStatementForExecutionCommand = connectionToDatabaseNetwork.prepareStatement(deleteCommandStringForDatabase)) {
+                preparedStatementForExecutionCommand.executeUpdate();
+            }
+        } catch (SQLException exceptionFromDatabaseNetworkError) {
+            // Throw the data access exception so server handler gives 500 error code for bad connection
+            throw new DataAccessException(exceptionFromDatabaseNetworkError.getMessage());
+        }
     }
 }
