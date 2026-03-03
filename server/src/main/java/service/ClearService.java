ce8e43f701b9ca8e848aae15bb7ea5d5e6691cd8
cyber-sec0 <91571849+cyber-sec0@users.noreply.github.com>
2026-03-03 15:22:23 -0800

    .

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -3,26 +3,35 @@ package service;
 import dataaccess.MemoryAuthDao;
 import dataaccess.MemoryGameDao;
 import dataaccess.MemoryUserDao;
+import dataaccess.DataAccessException;
 
 /**
- * This service is responsible for the nuking of the database.
- * It calls the clear method on every data access object.
+ * This class handles the operation to clear the whole system database.
+ * It connects the handler requests to the database access objects.
  */
 public class ClearService {
 
-    private final MemoryUserDao uDao;
-    private final MemoryGameDao gDao;
-    private final MemoryAuthDao aDao;
+    private final MemoryUserDao userDatabaseAccessObjectTool;
+    private final MemoryGameDao gameDatabaseAccessObjectTool;
+    private final MemoryAuthDao authDatabaseAccessObjectTool;
 
-    public ClearService(MemoryUserDao u, MemoryGameDao g, MemoryAuthDao a) {
-        this.uDao = u;
-        this.gDao = g;
-        this.aDao = a;
+    // This constructor initialize the tools for the clear service
+    public ClearService(MemoryUserDao userDaoObjectParameter, MemoryGameDao gameDaoObjectParameter, MemoryAuthDao authDaoObjectParameter) {
+        this.userDatabaseAccessObjectTool = userDaoObjectParameter;
+        this.gameDatabaseAccessObjectTool = gameDaoObjectParameter;
+        this.authDatabaseAccessObjectTool = authDaoObjectParameter;
     }
 
-    public void clearEverything() {
-        uDao.clear();
-        gDao.clear();
-        aDao.clear();
+    /**
+     * This function erases everything from the database tables.
+     * @throws DataAccessException If the database fails to execute.
+     */
+    public void clearEverything() throws DataAccessException {
+        // Erasing the user data from table so we have empty space
+        userDatabaseAccessObjectTool.clear();
+        // Erasing the game data from table so we have empty space
+        gameDatabaseAccessObjectTool.clear();
+        // Erasing the auth data from table so we have empty space
+        authDatabaseAccessObjectTool.clear();
     }
 }
