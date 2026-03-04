b1bd68c8b69bcdb387d7a347716041f952fb43de
cyber-sec0 <91571849+cyber-sec0@users.noreply.github.com>
2026-03-03 17:00:51 -0800

    .

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,117 +1,120 @@
 package service;
 
-import chess.ChessGame;
 import dataaccess.MemoryAuthDao;
 import dataaccess.MemoryGameDao;
 import dataaccess.DataAccessException;
 import model.AuthData;
 import model.GameData;
+import chess.ChessGame;
 import java.util.Collection;
+import java.util.Objects;
 import java.util.Random;
 
 /**
- * This service handles all the game related operations.
- * It is very important for the gameplay functionality.
+ * The GameService is the class that contains logic for game operations.
+ * It uses the data access objects to save things in database.
  */
 public class GameService {
 
-    private final MemoryGameDao gameDatabaseAccess;
-    private final MemoryAuthDao authCheckerDao;
+    private final MemoryGameDao gameDatabaseAccessObjectTool;
+    private final MemoryAuthDao authDatabaseAccessObjectTool;
 
-    public GameService(MemoryGameDao gameDao, MemoryAuthDao authDao) {
-        this.gameDatabaseAccess = gameDao;
-        this.authCheckerDao = authDao;
+    // This constructor initialize the tools for the game service
+    public GameService(MemoryGameDao gameDaoObjectParameter, MemoryAuthDao authDaoObjectParameter) {
+        this.gameDatabaseAccessObjectTool = gameDaoObjectParameter;
+        this.authDatabaseAccessObjectTool = authDaoObjectParameter;
     }
 
     /**
-     * This function creates a new game with a random ID.
-     * It tries to find a unique ID by generating random numbers.
-     * @param authToken The auth token.
-     * @param gameName The name of the game.
-     * @return The new game ID.
-     * @throws DataAccessException If failed.
+     * This method lists all the games from the database.
+     * @param authTokenStringValue The token string to check authorization.
+     * @return Collection of games.
+     * @throws DataAccessException If token is bad or database fails.
      */
-    public int createGame(String authToken, String gameName) throws DataAccessException {
-        AuthData verifier = authCheckerDao.getAuth(authToken);
-        if (verifier == null) {
+    public Collection<GameData> listGames(String authTokenStringValue) throws DataAccessException {
+        AuthData existingTokenFromDatabaseTable = authDatabaseAccessObjectTool.getAuth(authTokenStringValue);
+
+        // Checking if the token from the user is fake or null so we block
+        if (existingTokenFromDatabaseTable == null) {
             throw new DataAccessException("Error: unauthorized");
         }
-        if (gameName == null) {
-            throw new DataAccessException("Error: bad request");
-        }
 
-        Random numberGenerator = new Random();
-        int newGameIdInteger = Math.abs(numberGenerator.nextInt());
-
-        // This loop makes sure the ID is not already used in the database
-        while (gameDatabaseAccess.getGame(newGameIdInteger) != null) {
-            newGameIdInteger = Math.abs(numberGenerator.nextInt());
-        }
-
-        GameData freshGame = new GameData(newGameIdInteger, null, null, gameName, new ChessGame());
-        gameDatabaseAccess.createGame(freshGame);
-
-        return newGameIdInteger;
-    }
-
-    public Collection<GameData> listGames(String authToken) throws DataAccessException {
-        AuthData verifier = authCheckerDao.getAuth(authToken);
-        if (verifier == null) {
-            throw new DataAccessException("Error: unauthorized");
-        }
-        return gameDatabaseAccess.listGames();
+        return gameDatabaseAccessObjectTool.listGames();
     }
 
     /**
-     * This method allows a user to join a game.
-     * It checks colors and availability.
-     * @param authToken The token.
-     * @param playerColor The color to join.
-     * @param gameIdTarget The game ID.
-     * @throws DataAccessException If joining fails.
+     * This method creates a new game in the database.
+     * @param authTokenStringValue The token string.
+     * @param newGameNameStringValue The name of game.
+     * @return The numeric ID of the new game.
+     * @throws DataAccessException If token is bad or database fails.
      */
-    public void joinGame(String authToken, String playerColor, int gameIdTarget) throws DataAccessException {
-        AuthData verifier = authCheckerDao.getAuth(authToken);
-        if (verifier == null) {
+    public int createGame(String authTokenStringValue, String newGameNameStringValue) throws DataAccessException {
+        AuthData existingTokenFromDatabaseTable = authDatabaseAccessObjectTool.getAuth(authTokenStringValue);
+
+        // Checking if the token from the user is fake or null so we block
+        if (existingTokenFromDatabaseTable == null) {
             throw new DataAccessException("Error: unauthorized");
         }
 
-        GameData gameFound = gameDatabaseAccess.getGame(gameIdTarget);
-        if (gameFound == null) {
+        int randomNumericIdForNewGame = Math.abs(new Random().nextInt());
+        GameData newlyCreatedGameObject = new GameData(randomNumericIdForNewGame, null, null, newGameNameStringValue, new ChessGame());
+
+        gameDatabaseAccessObjectTool.createGame(newlyCreatedGameObject);
+        return randomNumericIdForNewGame;
+    }
+
+    /**
+     * This method adds a player to an existing game in the database.
+     * @param authTokenStringValue The token string.
+     * @param colorStringValue The color chosen.
+     * @param gameNumericIdValue The game ID.
+     * @throws DataAccessException If validations fail or database fails.
+     */
+    public void joinGame(String authTokenStringValue, String colorStringValue, int gameNumericIdValue) throws DataAccessException {
+        AuthData existingTokenFromDatabaseTable = authDatabaseAccessObjectTool.getAuth(authTokenStringValue);
+
+        // Checking if the token from the user is fake or null so we block
+        if (existingTokenFromDatabaseTable == null) {
+            throw new DataAccessException("Error: unauthorized");
+        }
+
+        GameData existingGameFromDatabaseTable = gameDatabaseAccessObjectTool.getGame(gameNumericIdValue);
+
+        // Checking if the game does not exist in database so we give error
+        if (existingGameFromDatabaseTable == null) {
             throw new DataAccessException("Error: bad request");
         }
 
-        if (playerColor == null) {
-            throw new DataAccessException("Error: bad request");
-        }
+        String usernameFromTokenString = existingTokenFromDatabaseTable.username();
+        String currentWhiteUsernameString = existingGameFromDatabaseTable.whiteUsername();
+        String currentBlackUsernameString = existingGameFromDatabaseTable.blackUsername();
 
-        String usernameOfPlayer = verifier.username();
-        String newWhiteUser = gameFound.whiteUsername();
-        String newBlackUser = gameFound.blackUsername();
-
-        // Used guard clauses to prevent deep nesting code
-        if (playerColor.equals("WHITE")) {
-            if (newWhiteUser != null) {
+        // Checking if the color is white so we assign user to white
+        if (Objects.equals(colorStringValue, "WHITE")) {
+            // Checking if the white player is already taken
+            if (currentWhiteUsernameString != null) {
                 throw new DataAccessException("Error: already taken");
             }
-            newWhiteUser = usernameOfPlayer;
-        } else if (playerColor.equals("BLACK")) {
-            if (newBlackUser != null) {
+            currentWhiteUsernameString = usernameFromTokenString;
+        } else if (Objects.equals(colorStringValue, "BLACK")) {
+            // Checking if the black player is already taken
+            if (currentBlackUsernameString != null) {
                 throw new DataAccessException("Error: already taken");
             }
-            newBlackUser = usernameOfPlayer;
+            currentBlackUsernameString = usernameFromTokenString;
         } else {
             throw new DataAccessException("Error: bad request");
         }
 
-        GameData updatedGameObject = new GameData(
-                gameFound.gameID(),
-                newWhiteUser,
-                newBlackUser,
-                gameFound.gameName(),
-                gameFound.game()
+        GameData updatedGameObjectForDatabase = new GameData(
+                existingGameFromDatabaseTable.gameID(),
+                currentWhiteUsernameString,
+                currentBlackUsernameString,
+                existingGameFromDatabaseTable.gameName(),
+                existingGameFromDatabaseTable.game()
         );
 
-        gameDatabaseAccess.updateGame(updatedGameObject);
+        gameDatabaseAccessObjectTool.updateGame(updatedGameObjectForDatabase);
     }
 }
