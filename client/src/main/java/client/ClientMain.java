c4a9659bbebb1b1e604b4523806e51816113ac2a
cyber-sec0 <91571849+cyber-sec0@users.noreply.github.com>
2026-03-12 10:43:42 -0700

    .

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -2,9 +2,10 @@ package client;
 
 import chess.*;
 
-public class ClientMain {
-    public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+public class ClientMain { // The main class for the client application project that run the system logic
+    public static void main(String[] args) { // The main function that start the execution of the program loop to not stop running
+        System.out.println("♕ 240 Chess Client: Started"); // Print the start message in the terminal screen
+        TerminalReplUserInterface loopObjectInstanceForRunningTheApp = new TerminalReplUserInterface(8080); // Create the loop object instance using the port integer
+        loopObjectInstanceForRunningTheApp.runTheProgramLoop(); // Call the run method to begin asking for inputs continuously
     }
 }
